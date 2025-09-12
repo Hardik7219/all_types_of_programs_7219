36ebc5f9ed3e741b6a75f81fce1210ecc8ca6a2c
Hardik7219 <hendreogaming@gmail.com>
2025-09-12 15:05:19 +0530

    Add files via upload

---
+++ C/Practice/1_0_1_0pattern.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+    int n,l=0;
+    scanf("%d",&n);
+    for( int i=1; i<=n; i++)
+    {    
+        for(int j=1; j<=i; j++)
+        { 
+            if(l%2==0)
+            {
+            printf(" 1");
+            }
+            else
+            {
+            printf(" 0");
+            }
+            l++;
+        }
+        
+        printf("\n");
+     }
+     }   
