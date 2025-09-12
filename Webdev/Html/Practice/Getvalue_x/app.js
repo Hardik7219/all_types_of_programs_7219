7015799a6cc885e0408c80398dc15a24ef7eff67
Hardik7219 <hendreogaming@gmail.com>
2025-09-12 14:33:20 +0530

    Add files via upload

---
+++ Webdev/Html/Practice/Getvalue_x/app.js
@@ -0,0 +1,8 @@
+let t1=document.querySelector("#t1");
+let t2=document.querySelector("#t2");
+let ans=document.querySelector("#ans")
+function button() {
+   let a=parseInt(t1.value);
+ let b=parseInt(t2.value);
+ ans.innerText=a+b;
+}
