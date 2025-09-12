2d164877caf4508454d1c4cc55984669bf4c6780
Hardik7219 <hendreogaming@gmail.com>
2025-09-12 14:41:12 +0530

    Add files via upload

---
+++ Webdev/Html/Practice/Project_test/p.js
@@ -0,0 +1,11 @@
+const img = document.querySelector(".i");
+img.addEventListener("click", () => {
+  let dis=document.createElement("h1");
+  dis.textContent="img 1";
+ let dis2=document.createElement("h3");
+dis2.textContent="img 2";
+  
+document.body.appendChild(dis);
+document.body.appendChild(dis2);
+});
+let =`hellp ${variable name}`;
