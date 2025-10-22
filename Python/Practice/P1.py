fc4d4a9ef2954fb3836e55c8e8be9777594e1c4b
Hardik7219 <hendreogaming@gmail.com>
2025-10-22 09:55:15 +0530

    formatter python file

--- Python/Practice/P1.py
+++ Python/Practice/P1.py
@@ -1,35 +1,34 @@
-import time 
+import time
 import os
 
-#first
-#cols={(0,0):' '} 
-def show(height,width):
-    cols={}
+# first
+# cols={(0,0):' '}
+
+
+def show(height, width):
+    cols = {}
  # i=height
  # j=width
-    for i in range(0,height*2):
-         for j in range (0,width*2):
-             if j==0 or j==width*2-1 or i==0 or i==height*2-1:
-               	print("*",end="")
-               	cols[(i,j)]="*"
-              else:
-        
-              	 print(" ",end="")
-           	    cols[(i,j)]=" "
-          	 time.sleep(0.007)
-        
-    	  print(" ")  
-      
-      	time.sleep(0.007)
-    
-#second
+    for i in range(0, height*2):
+        for j in range(0, width*2):
+            if j == 0 or j == width*2-1 or i == 0 or i == height*2-1:
+                print("*", end="")
+                cols[(i, j)] = "*"
+            else:
+                print(" ", end="")
+                cols[(i, j)] = " "
+        time.sleep(0.007)
 
+    print(" ")
 
-    for i in range(0,height*2):
-     	
-        for j in range(0,width*2):
-        	print(cols[(i,j)],end="")
+    time.sleep(0.007)
+
+# second
+
+    for i in range(0, height*2):
+        for j in range(0, width*2):
+            print(cols[(i, j)], end="")
         print(" ")
-         	
-show (5,5)        	
-      
+
+
+show(5, 5)
